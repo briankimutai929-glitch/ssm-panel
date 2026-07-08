54c7c5bdfeb9ec8296d7005959ff1944a1991c87
briankimutai929-glitch <briankimutai929@gmail.com>
2026-07-08 21:13:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,4 +21,21 @@ app.get("/", (req, res) => {
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("SSM Panel API Running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
